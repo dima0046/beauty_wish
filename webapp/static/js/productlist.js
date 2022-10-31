cea8d9e60674d7ebf3fca894b0b6297bf317efdc
dima0046 <dima0046@gmail.com>
2022-10-31 22:53:08 +0300

    product list 3. some fixes

--- webapp/static/js/productlist.js
+++ webapp/static/js/productlist.js
@@ -1,13 +1,15 @@
 function showList(e) {
-    var $gridCont = $('.grid-container');
+    var $gridCont = $(".grid-container");
     e.preventDefault();
-    $gridCont.hasClass('list-view') ? $gridCont.removeClass('list-view') : $gridCont.addClass('list-view');
+    $gridCont.hasClass("list-view")
+      ? $gridCont.removeClass("list-view")
+      : $gridCont.addClass("list-view");
   }
   function gridList(e) {
-    var $gridCont = $('.grid-container')
+    var $gridCont = $(".grid-container");
     e.preventDefault();
-    $gridCont.removeClass('list-view');
+    $gridCont.removeClass("list-view");
   }
   
-  $(document).on('click', '.btn-grid', gridList);
-  $(document).on('click', '.btn-list', showList);
+  $(document).on("click", ".btn-grid", gridList);
+  $(document).on("click", ".btn-list", showList);
